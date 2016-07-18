6129f7e024d50a1e97436c1988ff825ce66e48d6
Will Hui <will@willhui.com>
2016-07-18 11:24:00 +0800

    fixed fb login window and redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  mount DeviseTokenAuth::Engine => "/auth"
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    omniauth_callbacks:  'omniauth_callbacks'
+  }
+
   get '/signup', to: 'auth#signup'
   get '/login', to: 'auth#login'
   get '/secret', to: 'statics#secret'
