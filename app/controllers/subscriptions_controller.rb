6136593dae5f88048f174f27ef5bfa83892b75aa
Fiona <fionacheung224@gmail.com>
2016-07-18 16:52:17 +0800

    subscription_controller added add route

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,13 +1,17 @@
 class SubscriptionsController < ApplicationController
   def index
 
-    url = 'http://www.economist.com/sections/international/rss.xml'
-    @feed = Feedjira::Feed.fetch_and_parse url
+    # url = 'http://www.economist.com/sections/international/rss.xml'
+    # @feed = Feedjira::Feed.fetch_and_parse url
   end
-  @post = current_user.posts.new(subscription_params)
 
   def add
     bookmark = current_user.bookmarks.new(subscription_params)
+    if bookmark.save
+      render json: bookmark
+    else
+      render json: @post.errors.messages, status: 400
+    end
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
 
   get '/subscriptions', to: 'subscriptions#index'
-  get '/subscriptions/add', to: 'subscriptions#add'
+  post '/subscriptions/add', to: 'subscriptions#add'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
