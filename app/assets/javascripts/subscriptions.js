b05f283db9b581e5c4880b797e8a0e97bbdb7f5d
nalgritz <nalgritz9270@gmail.com>
2016-07-19 16:03:59 +0800

    remove modal borders, leave modal styling to will

--- app/assets/javascripts/subscriptions.js
+++ app/assets/javascripts/subscriptions.js
@@ -1,18 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function(){
-  if (!$('body').hasClass('ajax')) { return false; }
-console.log("subscription loaded")
-  var addFeeds = {
-    bindAddFeedsClick: function () {
-      $('#search').on('click', function (e) {
-        e.preventDefault();
-
-        $('#search-modal').modal('show');
-      });
-    },
-    init: function () {
-      this.bindAddFeedsClick();
-    }
-  }
-});
