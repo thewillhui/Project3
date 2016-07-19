4b501bd87fe2bf18c6fdda10e64a09cb0b8e8986
nalgritz <nalgritz9270@gmail.com>
2016-07-19 11:16:26 +0800

    render stock images for feeds w/o images

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -25,6 +25,8 @@ class SubscriptionsController < ApplicationController
       }
       @subscriptions.push(subscription);
     end
+    # user subscription is sorted by rss source and data.published. Any ways to sort out of data.published?
+
     # url = 'http://www.economist.com/sections/international/rss.xml'
   end
 
