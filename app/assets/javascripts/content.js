74a9e155b514537d62cf08edbc2724230eb2624d
Will Hui <will@willhui.com>
2016-07-22 00:02:30 +0800

    fixed conflicts

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -150,13 +150,10 @@ $(document).ready(function() {
           listhtml = '';
           $.each(data, function(key, items){
             items.forEach(function(item){
-              listhtml = '<li class="sub" data-id="' + item.id + '"><img class="manage-logo-url" src="'+item.logoUrl+'">' + item.title + '</li>';
-              $('#'+key+'-manage-folder').append(listhtml);
-            })
-
-          })
-          optionHtml = '<span>' +
-            '<div class="dropdown">' +
+              listhtml = '<li class="sub" data-id="' + item.id + '">'+ '<div class="col-xs-12">' +'<div class="col-xs-6">' +
+              '<img src="'+item.logoUrl+'">' + item.title + '</div>' +
+              '<div class="subscription-btns col-xs-6">' +
+            '<div class="dropdown pull-right">' +
             '<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Change folder' +
             '<span class="caret"></span></button>' +
             '<ul class="dropdown-menu">' +
